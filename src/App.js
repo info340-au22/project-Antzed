27321b201c2d9e7fac7901294cab05b688f19fe2
Ben-Ferry <bbferry@uw.edu>
2022-11-18 20:13:19 -0800

    updated App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "./component/Header";
+
+export function App(props) {
+    return (
+        <div>
+            <body className="background">
+                <section class="home-section">
+                    <Header />
+                </section>
+            </body>
+        </div>
+    )
+}
