a075d0263d8ea9aa05f5b4f5f1337e1b36bc753a
Fatima Dineh <fatimadineh@Fatimas-MacBook-Air-2.local>
2022-11-22 10:59:00 -0800

    shop

--- src/component/Cardshop.js
+++ src/component/Cardshop.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 export default function Cardshop(props){
+
+    const cards = props.cards;
+
     return(
         <section className="product_buy">
         <div className="row d-flex">
 
+        { cards.map(card => (
+            <CardItem 
+              {...card}
+            />
+        ))}
+
+{/* 
         <div className="col-md-4 col-lg-3 d-flex mt-2">  
             <div className="card" style={{width: '18rem'}}>
             <div className="card-body">
@@ -149,8 +159,27 @@ export default function Cardshop(props){
             <a href="#" className="btn btn-outline-dark">Buy</a>
     </div>
     </div>
-</div>
+</div> */}
 </div> 
 </section>
     )
+ }
+
+
+
+ export const CardItem = (props) => {
+     const { title, description, price, imageName, store } = props;
+     return (
+        <div className="col-md-4 col-lg-3 d-flex mt-2">  
+        <div className="card" style={{width: '18rem'}}>
+        <div className="card-body">
+            <img className="card-img-top pb-3" src={"../img/"+ imageName} alt="a logo of a man walking" height={'350px'}/>
+            <h2 className="card-title"> {title } </h2>
+            <p className="card-text"> {description} </p>
+            <p className="card-text">Price: {price} </p>
+            <a href={store} target="_blank" className="btn btn-outline-dark">Buy</a>
+    </div>
+    </div>
+    </div>
+     )
  }
