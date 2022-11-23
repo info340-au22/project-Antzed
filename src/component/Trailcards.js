d4493d9a5ecd53e27f32cc2a57fea7659230e66d
Ben-Ferry <bbferry@uw.edu>
2022-11-22 17:15:16 -0800

    updated trail page

---
+++ src/component/Trailcards.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function SingleCard(props) {
+    const cardData = props.cardObjData
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="d-flex col-auto col-md-6 col-xl-3 rounded mt-4">
+                    <div className="card">
+                        <img src={cardData.img} className=".card-img-top" alt={cardData.title} />
+                        <div classname="card-body">
+                            <h2>{cardData.title}</h2>
+                            <p>{cardData.description}</p>
+                            <SeeMoreButton />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function SeeMoreButton(props) {
+
+}
+export function TrailCards(props) {
+    const cardsData = props.cards
+    const singleCardData = cardsData.map((cardObj) => {
+        return (
+            <SingleCard cardObjData={cardObj} key={cardObj.title}/>
+        )
+    });
+    return (
+        <div></div>
+    )
+}
--- src/pages/TrialPage.js
+++ src/pages/TrialPage.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import NavBar from "../component/NavBar";
 import Footer from "../component/Footer";
+import { TrailCards } from "../component/Trailcards.js";
+import { CardSelect } from "../component/Trailfilter.js";
 
 export default function TrailPage(props) {
+    const [displayedCards, setDisplayedCards] = useState(props.trailData)
+
+    const applyFilter = (diff) => {
+        if (diff == '') {
+            setDisplayedCards(props.trailData)
+          }else {
+            const difficulty = props.trailData.filter((element) => {
+              return element.difficulty === diff
+            });
+            setDisplayedCards(difficulty)
+        }
+    }
+    const uniqueHikeDiff = [...new Set(props.trailData.reduce((all, current) => {
+        return all.concat([current.difficulty]);
+      }, []))].sort();
+    console.log(uniqueHikeDiff)
     return (
         <div>
             <NavBar pageName = "Trail"/>
-            <h1>Trail Page</h1>
+            <main>
+                <h1>Trail Page</h1>
+                <CardSelect hikeOptions={uniqueHikeDiff} applyCallBackFilter={applyFilter}/>
+                <TrailCards cards={displayedCards} />
+            </main>
+
             <Footer isInherit={true}/>
         </div>
     )
